fc05038eec42683b3d61acbe7680adbc18d7fbca
Thainara Januario <112710828+thainarajanuario@users.noreply.github.com>
2022-12-07 11:48:20 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { Postagem } from './postagem/entities/postagem.entity';
-import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
-import { TemaModule } from './tema/tema.module';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { UsuarioModule } from './usuario/usuario.module';
+import { Postagem } from './Postagem/entities/postagem.entity';
+import { PostagemModule } from './Postagem/postagem.module';
+import { Tema } from './Tema/entities/tema.entity';
+import { TemaModule } from './Tema/tema.module';
+import { Usuario } from './Usuario/entities/usuario.entity';
+import { UsuarioModule } from './Usuario/usuario.module';
 
 
 @Module({
