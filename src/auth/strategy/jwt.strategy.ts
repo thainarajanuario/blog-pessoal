a4f992e58df37d9e96d3c4b7ea2c73d87dce7414
Thainara Januario <112710828+thainarajanuario@users.noreply.github.com>
2022-12-07 12:00:17 -0300

    Update jwt.strategy.ts

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -4,17 +4,16 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { jwtConstants } from "../constants/constants";
 
 @Injectable()
-export class JwtStrategy  extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(){
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false, 
-            secretOrKey: jwtConstants.secret  
+            ignoreExpiration: false,
+            secretOrKey: jwtConstants.secret
         })
     }
 
-async validate(payload: any) {
-    return {userId: payload.sub, username: payload.username}
-}
-
+    async validate(payload: any) {
+        return {userId: payload.sub, username: payload.username}
+    }
 }
