a35701af765bb5fc9c148c5717609e3d644a770e
Thainara Januario <112710828+thainarajanuario@users.noreply.github.com>
2022-12-07 12:19:50 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+import { validate } from 'class-validator';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  
   const config = new DocumentBuilder()
-  .setTitle('Blog Pessoal')
-  .setDescription('Projeto do Blog Pessoal')
-  .setContact('Generation Brasil', 'www.genbr.com.br', 'gigitata199427@gmail.com')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('/swagger', app, document)
+    .setTitle('Blog Pessoal')
+    .setDescription('Blog pessoal da Thai')
+    .setContact('Generation Brasil', 'www.genbr.com.br', 'gigitata199427@gmail.com')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/swagger', app, document)
 
 
-  process.env.TZ = '-03:00';
+
+  process.env.TZ = '-03:00'
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors()
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
+
